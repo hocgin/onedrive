56aeecd06749c4aa6382cc304269ec11c8ebbd73
hocgin <examle@mail.com>
2025-01-07 09:42:16 +0800

    dev

--- config/api.config.js
+++ config/api.config.js
@@ -10,12 +10,12 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'f23f9918-14a5-46f1-9f74-13e57273b8f3',
-  obfuscatedClientSecret: 'U2FsdGVkX1/hHUr0qvtP2dAJuY9MqdXfryBEQW3FkN4AI6MGmoxTOxEANDejnriD/4nJXcg4Lv4sSXI0O5Lf4w==',
+  clientId: 'aace4d2c-dc94-459c-b846-7a317c0b4f8b',
+  obfuscatedClientSecret: 'U2FsdGVkX19nYVxcVEa1fmYb2QcNAvBGjY1a1fM99Aeo0IrX3pG8nmhv1sZfFltMovX5uH2vHp6Gz35kIRoY/w==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
-  redirectUri: 'http://localhost',
+  redirectUri: 'https://aipan.app',
 
   // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
   // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
