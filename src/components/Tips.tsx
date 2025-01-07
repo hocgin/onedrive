ce092f2c83349d5f360b5ca32b5628d5032455b6
hocgin <examle@mail.com>
2025-01-07 11:51:02 +0800

    dev

--- src/components/Tips.tsx
+++ src/components/Tips.tsx
@@ -1,12 +1,16 @@
 import { useClipboard } from 'use-clipboard-copy'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Tips = ({ mpname }: { mpname: string }) => {
-  const clipboard = useClipboard()
+  const clipboard = useClipboard({ copiedTimeout: 1000 })
   return (
     <div>
       关注{' '}
-      <span className={'text-red-600'} onClick={() => clipboard.copy(mpname)}>
-        {mpname}
+      <span className={'flex-row space-x-1'}>
+        <span className={'text-red-600'} onClick={() => clipboard.copy(mpname)}>
+          {mpname}
+        </span>
+        {clipboard.copied ? <FontAwesomeIcon icon="check" /> : <FontAwesomeIcon icon="copy" />}
       </span>{' '}
       获取下载密码
     </div>
